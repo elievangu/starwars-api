adb01308cbff4fcb54ddf459682a9f4d2803f412
Elie Vangu <vangu.elie@gmail.com>
2018-12-18 11:35:33 +0100

    settings

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+//npm import
+import React from 'react';
+
+
+//local import
+
+
+class App extends React.Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+//npm import
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//local import
+import './index.css';
+import App from './Components/App';
+
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
