5668abc051a5ef29e2c593cdfeec98817829516d
Elie Vangu <vangu.elie@gmail.com>
2018-12-19 15:33:58 +0100

    Components and containers handling

--- src/Containers/FooterApp.js
+++
@@ -1,5 +0,0 @@
-//npm import
-import { connect } from 'react-redux';
-
-//local import
-import Footer from '../Components/UI/Footer';
--- src/Containers/NavApp.js
+++
@@ -1,5 +0,0 @@
-//npm import
-import { connecet } from 'react-redux';
-
-//local import
-import Nav from '../Components/UI/Nav';
