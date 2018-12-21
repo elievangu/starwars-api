05fbb7fd4e3a70f64028e1083537303583ab6c53
Elie Vangu <vangu.elie@gmail.com>
2018-12-21 01:29:42 +0100

    Aside component CSS handling

--- src/Components/UI/Aside.js
+++ src/Components/UI/Aside.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 const Aside = () => {
   return (
-    <div className="row">
+    <div className="row aside">
         <div className="col s12 m7">
           <aside className="card-panel red">
             <span className="white-text">Pellentesque mi quam, dignissim eu purus sit amet, sodales molestie quam. Curabitur blandit, sapien sed porta venenatis, arcu ante mollis eros, vitae suscipit risus felis sed ligula. Nunc pellentesque eros non pharetra tincidunt. Maecenas finibus dapibus tellus, ac porttitor purus pulvinar eget. Nulla porttitor, nulla nec efficitur condimentum, ante lorem mattis enim, id vehicula ipsum nunc eget sapien. Mauris in ultricies lectus. Fusce lacus est, faucibus sit amet elementum eget, molestie sed lectus. Duis mattis risus mauris, imperdiet eleifend dui suscipit vitae.</span>
